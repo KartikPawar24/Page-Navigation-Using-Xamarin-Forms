d39e26c358e0b2c250e0019fcf6f5cd3516a25ab
Kartik Pawar <33437982+KartikPawar24@users.noreply.github.com>
2018-06-06 19:23:33 +0530

    Add files via upload

---
+++ Page_Navigation_Using_Xamarin_Forms/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Page_Navigation_Using_Xamarin_Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Onbtn_Clicked(object sender,EventArgs e)
+        {
+            await Navigation.PushAsync(new SecondPage());
+        }
+    }
+}
---
+++ Page_Navigation_Using_Xamarin_Forms/SecondPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Page_Navigation_Using_Xamarin_Forms
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnBtn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page_Navigation_Using_Xamarin_Forms.MainPage());
+        }
+    }
+}
